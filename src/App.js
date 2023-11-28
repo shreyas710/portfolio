70e94ccebba50c7412a01881e83806c21e64bd04
shreyas710 <shreyasnk5321@gmail.com>
2023-11-27 23:19:07 -0800

    react loader

--- src/App.js
+++ src/App.js
@@ -4,8 +4,38 @@ import Home from "./components/Home/Home";
 import Experience from './components/Experience/Experience';
 import Project from './components/Project/Project';
 import Contact from './components/Contact/Contact';
+import { useEffect, useState } from 'react';
+import ReactLoading from "react-loading";
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000)
+  })
+
+  if(loading) {
+    return (
+      <div className="App">
+        <div className="loadingContent row">
+          <div className="col-4"></div>
+          <div className="loadingLogo col-4">
+              <ReactLoading
+              type={"bars"}
+              color={"#f1365d"}
+              height={200}
+              width={200}
+            />
+          </div>
+          <div className="col-4"></div>
+        </div>
+      </div>
+    )
+  } 
+
   return (
     <div className="App">
         <Navbar/>
