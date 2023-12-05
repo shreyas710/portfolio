c00884267ac7873f5dd53290b645a6e30fef83f0
shreyas710 <shreyasnk5321@gmail.com>
2023-12-04 18:48:18 -0800

    add loading true

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ReactLoading from "react-loading";
 
 function App() {
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
