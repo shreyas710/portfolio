97713149261be8731a9bff21985dd95ee3ebeec5
shreyas710 <shreyasnk5321@gmail.com>
2023-12-20 14:08:04 +0530

    drive link updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -50,7 +50,7 @@ const Home = () => {
       </div>
 
       <div className="resumeButton row">
-        <a href= 'https://drive.google.com/file/d/1imPKzILpPTJ2eZNR_Oedy_DipMXWbFK2/view?usp=sharing' target='_blank' rel='noreferrer noopener' type='button'>
+        <a href= 'https://drive.google.com/file/d/1B0BZ53MvsyGE_11tdUMvua9BkX41XqOD/view?usp=sharing' target='_blank' rel='noreferrer noopener' type='button'>
           View Resume
         </a>
       </div>
