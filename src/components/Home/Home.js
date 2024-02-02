06b9006114fc725debae7c5d77fc075ec0387680
shreyas710 <shreyasnk5321@gmail.com>
2024-02-02 00:06:54 -0800

    modified home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,46 +7,57 @@ const Home = () => {
   return (
     <div className='home container'>
       <div className="animation row">
-        <TypeAnimation
-          sequence={[
-            'Hi, I am Shreyas Kulkarni.',
-          ]}
-          wrapper="div"
-          speed={5}
-          style={{ color:'#f1356d', fontSize: '5em', display: 'inline-block' }}
-          repeat={0}
-        />
+        <p style={{fontSize: "5em"}}>Shreyas Kulkarni.</p>
       </div>
       
 
       <div className="extra row">
         <div className="aboutme col-md-12 ">
+          <TypeAnimation
+            sequence={[
+                'Software Engineer',
+                1500,
+                'Full Stack Developer',
+                1500,
+                'Dev Ops Engineer',
+                1500
+              ]}
+            wrapper="div"
+            speed={10}
+            deletionSpeed={60}
+            style={{ color:'#f1356d', fontSize: '2em', display: 'inline-block' }}
+            repeat={Infinity}
+          /> 
+          <br />
+          <br />
+          <br />
           <h5>I am a Computer Science Graduate Student at the University of Southern California.<br></br>
           I have a good grasp of Data Structures and Algorithms. I have good experience in the fields of <br />frontend and backend development.</h5>
-
           <br />
           <br />
           <br />
-
         </div>
         <div className="profileLinks row">
           <div className="github col-1">
             <a href="https://github.com/shreyas710" target='_blank' rel='noreferrer noopener'>
-             <img src={require("../../assets/github-antiquewhite.png")} width={"40px"} height={"40px"} alt="" />
+             <img src={require("../../assets/github-antiquewhite.png")} width={"35px"} height={"35px"} alt="" />
             </a>
           </div>
           <div className="linkedin col-1">
             <a href="https://www.linkedin.com/in/shreyasnk532" target='_blank' rel='noreferrer noopener'>
-             <img src={require("../../assets/linkedin-antiquewhite.png")} width={"40px"} height={"40px"} alt="" />
+             <img src={require("../../assets/linkedin-antiquewhite.png")} width={"35px"} height={"35px"} alt="" />
             </a>
           </div>
           <div className="mail col-1">
             <a href="mailto:shreyasnk532@gmail.com" target='_blank' rel='noreferrer noopener'>
-             <img src={require("../../assets/envelope-solid-antiquewhite.png")} width={"40px"} height={"40px"} alt="" />
+             <img src={require("../../assets/envelope-solid-antiquewhite.png")} width={"35px"} height={"35px"} alt="" />
             </a>
           </div>
         </div>
       </div>
+
+      <br />
+      <br />
 
       <div className="resumeButton row">
         <a href= 'https://drive.google.com/file/d/1B0BZ53MvsyGE_11tdUMvua9BkX41XqOD/view?usp=sharing' target='_blank' rel='noreferrer noopener' type='button'>
