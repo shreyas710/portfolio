175414627a7b72187c05858b46e92d5f41870fc0
shreyas710 <shreyasnk5321@gmail.com>
2024-02-06 20:03:30 -0800

    dark mode based on device theme

--- src/App.js
+++ src/App.js
@@ -12,18 +12,33 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
 
+  const useThemeDetector = () => {
+    const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme());  
+    const mqListener = (e => {
+        setIsDarkTheme(e.matches);
+    });
+    
+    useEffect(() => {
+      const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+      darkThemeMq.addListener(mqListener);
+      return () => darkThemeMq.removeListener(mqListener);
+    }, []);
+    return isDarkTheme;
+  }
+
   const [loading, setLoading] = useState(true);
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(useThemeDetector());
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 1500)
+    }, 2000)
   })
 
   if(loading) {
     return (
-      <div className="App loadingContent" style={{color: 'antiquewhite'}}>
+      <div className="App loadingContent" style={{backgroundColor: darkMode ? "#222" : "antiquewhite"}}>
         <div className="spinner-border" role="status" style={{color: '#f1356d', width: '50px', height: '50px'}}>
           <span className="visually-hidden">Loading...</span>
         </div>
